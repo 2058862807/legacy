68ea245eddf4871db1dc334eacb8b2687ea45d74
Dustintylerjames <dustintylerjames@gmail.com>
2025-08-27 19:55:22 -0500

    Create page.tsx

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+return (
+<main>
+<h1 className="text-3xl font-bold mb-2">Dashboard</h1>
+<p className="text-gray-600">Pick a module in the top nav</p>
+</main>
+)
+}

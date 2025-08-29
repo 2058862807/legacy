11be79517e9ee3189731da6e81caaf46eb024ff4
2058862807 <dustintylerjames@gmail.com>
2025-08-29 17:25:04 -0500

    chore: reset web to minimal scaffold

--- web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-﻿import DocumentList from "@/components/Documents/DocumentList"
-export default function Page() {
-return (
-<main className="p-6">
-<h1 className="text-2xl font-semibold">Dashboard</h1>
-<DocumentList />
-</main>
-)
-}
--- web/components/Documents/DocumentList.tsx
+++
@@ -1,3 +0,0 @@
-﻿export default function DocumentList() {
-return <div>No documents yet</div>
-}

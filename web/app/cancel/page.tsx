f8adaf1b38d5fa09bc361f935d119f6fc9515e6e
2058862807 <dustintylerjames@gmail.com>
2025-08-29 16:00:28 -0500

    chore: remove duplicate root app, keep web/

--- web/app/cancel/page.tsx
+++
@@ -1,1 +0,0 @@
-export default function Cancel(){return <main className='p-8'>Payment canceled.</main>}
--- web/app/success/page.tsx
+++
@@ -1,1 +0,0 @@
-export default function Success(){return <main className='p-8'>Payment success. Check email for receipt.</main>}
